42b4fa47ef69635645083c5221af0e3840ed8abf
vbrazas <appleitunesstore11@gmail.com>
2019-01-31 20:04:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ft_ssl_md5 checker
+*Checker for the 42 ft_ssl_md5 project*
+
+`./ft_ssl_md5_checker` generates 5 files filled with 500 random binary characters, then compares output
+of `./ft_ssl_md5` and `openssl` programs
+
+## How to install ?
+
+	git clone https://github.com/millon15/ft_ssl_md5_checker.git
+	chmod +x ft_ssl_md5_checker.zsh
+
+## How to use ?
+
+	Usage: ./ft_ssl_md5_checker.zsh <path_to_your_ft_ssl_md5_executable>
+	Example: ./ft_ssl_md5_checker.zsh ft_ssl_md5_folder/ft_ssl_md5
---
+++ check_ssl_md5.zsh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+
+if [[ "$1" == "" ]]; then
+	echo -n "Enter path to ./ft_ssl_md5: "
+	read -r ft_ssl_md5
+else
+	ft_ssl_md5=$1
+fi
+
+RED='\e[0;31m'
+
+make -C $(dirname ft_ssl_md5)
+for l in {1..5}; do
+	FILENAME=.file_$l
+	chars_amm=$(($RANDOM%500))
+
+	echo > $FILENAME
+	for (( i = 1; i <= $chars_amm; i++ )); do
+		arr[$i]=$(($RANDOM%256));
+	done
+	for (( i = 1; i <= $chars_amm; i++ )); do
+		echo -n "\x$(printf %x ${arr[$i]})" >> $FILENAME
+	done
+
+	for cmd in {md5,sha256,sha512}; do
+		o_openssl=$(openssl $cmd $FILENAME)
+		o_ft_ssl_md5=$(./ft_ssl_md5/ft_ssl $cmd $FILENAME)
+		if diff <(echo ${o_openssl// /}) <(echo ${o_ft_ssl_md5// /}); then; else
+			echo "\n${RED}ERROR occured with file: $FILENAME"
+			exit 1
+		fi
+	done
+done
+rm -f .file_*
